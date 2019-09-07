3d064afb79bc44ff49a7444df2f553cad79fcf86
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-07 22:21:01 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,15 +5,22 @@ sum = num1 + num2puts sum
 end
 
 def subtraction(num1, num2)
-
+num1 = 10
+num2 = 5
+subtract = num1 - num2
+puts subtract
 end
 
 def division(num1, num2)
-
+num1 = 50
+num2 = 2
+divides = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+num1 = 4
+num2 = 30
+multiply = num1 * num2
 end
 
 def modulo(num1, num2)
